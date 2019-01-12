0be62636e9acbd08ec485a2432b8cc4f8ac24a54
swapnil283 <swapnil283@gmail.com>
2019-01-12 13:16:23 +0530

    Trial jj

--- app/src/main/java/com/example/swapnil/myapplication/SuccessFullmessage.java
+++ app/src/main/java/com/example/swapnil/myapplication/SuccessFullmessage.java
@@ -3,12 +3,11 @@ package com.example.swapnil.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GuestDetails extends AppCompatActivity {
+public class SuccessFullmessage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guest_details);
-
+        setContentView(R.layout.activity_success_fullmessage);
     }
 }
